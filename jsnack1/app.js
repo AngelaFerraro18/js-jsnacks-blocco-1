5819c7141835d4baa9c27e869234279392f47190
AngelaFerraro18 <angela.ferraro1802@gmail.com>
2025-02-27 12:15:31 +0100

    jsnack1 completed

---
+++ jsnack1/app.js
@@ -0,0 +1,14 @@
+/* jsnack1
+L’utente inserisce due numeri in successione, con due prompt.
+Il software stampa il maggiore.*/
+
+
+const prompt1 = parseInt(prompt('Inserisci un numero'));
+
+const prompt2 = parseInt(prompt('Inserisci un numero'));
+
+
+if (prompt1 > prompt2) {
+    console.log(prompt1)
+}
+console.log(prompt2);
